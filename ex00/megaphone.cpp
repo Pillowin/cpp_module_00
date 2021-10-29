d3430ee8086f227ce6984ed8da6fe66747369603
Antoine GAUTIER <gautier.antoine@gmx.com>
2021-10-29 15:40:01 +0200

    feat(ex00): megaphone done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,14 +6,23 @@
 /*   By: agautier <agautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 12:10:59 by agautier          #+#    #+#             */
-/*   Updated: 2021/10/29 14:54:00 by agautier         ###   ########.fr       */
+/*   Updated: 2021/10/29 15:37:13 by agautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	std::cout << "Hello World!" << std::endl;
-	return (1);
+	if (argc < 2)
+	{
+		std::cout << "* LOURD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (EXIT_SUCCESS);
+	}
+	for (int i = 1; i < argc; i++)
+		for (size_t j = 0; j < strlen(argv[i]); j++)
+			std::cout << (char)std::toupper(argv[i][j]);
+	std::cout << std::endl;
+	return (EXIT_SUCCESS);
 }
