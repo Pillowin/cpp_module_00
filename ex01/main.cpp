01ba6bb3e3217767e0182507d7117b0c7ee1629c
agautier <pillowin@gmx.com>
2021-11-03 16:09:45 +0100

    fix: remove TODO

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,15 +6,14 @@
 /*   By: agautier <agautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 16:24:05 by agautier          #+#    #+#             */
-/*   Updated: 2021/10/30 22:50:16 by agautier         ###   ########.fr       */
+/*   Updated: 2021/11/03 16:09:25 by agautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.h"
 #include "PhoneBook.hpp"
 
-// TODO: index should overflow
-int main(void)
+int	main(void)
 {
 	PhoneBook pb;
 
